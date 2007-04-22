60bdce46e45ada2710f338ce0873919825fc17c5
JoungKyun Kim <joungkyun@daum.net>
2007-04-22 17:39:33 +0000

    PHP NIS Extension

---
+++ test.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?
+if ( ! extension_loaded ('nis') )
+	dl ('nis.so');
+
+$user = '';
+$pass = '';
+$domain = '';
+$map = 'passwd.byname';
+
+if ( nis_auth ($user, $pass, $domain, $map) === FALSE )
+	echo "Auth Failed\n";
+else
+	echo "Auth Success\n";
+?>
